033214c2603abedcbc97bddf128b6bc4eb57018b
danidani83 <danidani83@proton.me>
2026-01-20 13:18:31 -0300

    Update urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -32,3 +32,13 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 >>>>>>> Samuel
+from django.contrib import admin
+from django.urls import path, include
+**from django.conf import settings**
+**from django.conf.urls.static import static**
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('comerciojusto.urls')), # Nome do seu App
+] 
+
